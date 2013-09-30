dd9d6029e9b82a7f008f4b86d58f80f58d6a8006
jfl913 <jfl913@163.com>
2013-09-30 09:43:10 +0800

    UIDevice Notification

---
+++ HeavyRotation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HeavyRotation
+//
+//  Created by jfl913 on 13-9-30.
+//  Copyright (c) 2013年 jfl913. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RotationAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RotationAppDelegate class]));
+    }
+}
